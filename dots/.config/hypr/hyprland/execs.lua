78efb5e0e44af34cde4808b6a633702837e9b9ec
azef <xennava@gmail.com>
2026-07-31 09:21:58 +0700

    refactor: simplify startup command initialization

--- dots/.config/hypr/hyprland/execs.lua
+++ dots/.config/hypr/hyprland/execs.lua
@@ -1,25 +1,31 @@
 -- put former exec-once commands inside the func and former exec commands outside
-hl.on("hyprland.start", function ()
+hl.on("hyprland.start", function()
+	local startup_commands = {
+		-- Bar, wallpaper
+		"$HOME/.config/hypr/hyprland/scripts/start_geoclue_agent.sh",
+		"qs -c $qsConfig",
+		"$HOME/.config/hypr/custom/scripts/__restore_video_wallpaper.sh",
 
-    -- Bar, wallpaper
-    hl.exec_cmd("$HOME/.config/hypr/hyprland/scripts/start_geoclue_agent.sh")
-    hl.exec_cmd("qs -c $qsConfig")
-    hl.exec_cmd("$HOME/.config/hypr/custom/scripts/__restore_video_wallpaper.sh")
+		-- Core components
+		"gnome-keyring-daemon --start --components=secrets",
+		"hypridle",
+		"dbus-update-activation-environment --all",
+		-- Delay to ensure the user systemd session is fully initialized.
+		"sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP",
 
-    -- Core components (authentication, lock screen, notification daemon)
-    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("dbus-update-activation-environment --all")
-    hl.exec_cmd("sleep 1 && dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP") -- Some fix idk
+		-- Audio
+		"easyeffects --hide-window --service-mode",
 
-    -- Audio
-    hl.exec_cmd("easyeffects --hide-window --service-mode")
+		-- Cursor
+		"hyprctl setcursor Bibata-Modern-Classic 24",
+	}
 
-    -- Clipboard: history
-    --hl.exec_cmd("wl-paste --watch cliphist store")
-    hl.exec_cmd("wl-paste --type text --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'")
-    hl.exec_cmd("wl-paste --type image --watch bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'")
+	for _, cmd in ipairs(startup_commands) do
+		hl.exec_cmd(cmd)
+	end
 
-    -- Cursor
-    hl.exec_cmd("hyprctl setcursor Bibata-Modern-Classic 24")
+	local cliphist_update = "bash -c 'cliphist store && qs -c $qsConfig ipc call cliphistService update'"
+	for _, t in ipairs({ "text", "image" }) do
+		hl.exec_cmd(("wl-paste --type %s --watch %s"):format(t, cliphist_update))
+	end
 end)
